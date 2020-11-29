a2cedca773c4313d1c8bb5d2d30a972ae4ffee8d
Daniel Moisa <danimoisa93@gmail.com>
2020-11-29 18:53:40 +0200

    config app

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -5,6 +5,7 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51',
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
-  const port = 3000;
 
+  const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
 }
