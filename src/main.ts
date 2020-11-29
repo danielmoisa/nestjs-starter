3e074914dc8c58ade447ea33a2ee85c386c086f5
Daniel Moisa <danimoisa93@gmail.com>
2020-11-29 19:19:31 +0200

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const serverConfig = config.get('server');
 
   const port = process.env.PORT || serverConfig.port;
-  const app = await NestFactory.create(AppModule);
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
